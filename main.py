978bcf2807c58ded988fc4185bc6af4e46343962
avjustice <iatsenko.av@gmail.com>
2023-05-09 19:02:57 +0300

    made authorization

--- main.py
+++ main.py
@@ -1,8 +1,54 @@
-from fastapi import FastAPI
+from fastapi import Depends, FastAPI
+
+from auth.db import User, create_db_and_tables
+from auth.schemas import UserCreate, UserRead, UserUpdate
+from auth.users import auth_backend, current_active_user, fastapi_users
 
 app = FastAPI()
+
+app.include_router(
+    fastapi_users.get_auth_router(auth_backend), prefix="/auth/jwt", tags=["auth"]
+)
+app.include_router(
+    fastapi_users.get_register_router(UserRead, UserCreate),
+    prefix="/auth",
+    tags=["auth"],
+)
+app.include_router(
+    fastapi_users.get_users_router(UserRead, UserUpdate),
+    prefix="/users",
+    tags=["users"],
+)
+
+
+@app.get("/authenticated-route")
+async def authenticated_route(user: User = Depends(current_active_user)):
+    return {"message": f"Hello {user.email}!"}
+
+
+@app.on_event("startup")
+async def on_startup():
+    # Not needed if you setup a migration system like Alembic
+    await create_db_and_tables()
+
+
+fake_users = [
+    {"id": 1, "role": "admin", "name": "Bob"},
+    {"id": 2, "role": "investor", "name": "John"},
+    {"id": 3, "role": "trader", "name": "Matt"},
+]
 
 
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
+
+
+@app.get("/users")
+async def get_users():
+    return fake_users
+
+
+@app.get("/users/{user_id}")
+async def get_user(user_id: int):
+    return [user for user in fake_users if user.get("id") == user_id]
